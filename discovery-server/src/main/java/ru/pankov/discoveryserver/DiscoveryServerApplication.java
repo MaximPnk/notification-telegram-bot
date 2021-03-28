69a1952fcdad772921927f470be4360eb062187e
Maxim <adm.mjkee@yandex.ru>
2021-03-28 18:16:27 +0300

    create eureka server module

---
+++ discovery-server/src/main/java/ru/pankov/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,17 @@
+package ru.pankov.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@EnableEurekaServer
+@PropertySource(value = "classpath:secret.properties")
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
